102b15fa8ab71f1719e75ad1447b78fe8a5465cf
bruno <brunofelippefaria@gmail.com>
2026-09-05 12:15:58 -0300

    feat: adiciona entityConfiguration

--- backend/BurgerX.Infrastructure/Persistence/AppDbContext.cs
+++ backend/BurgerX.Infrastructure/Persistence/AppDbContext.cs
@@ -6,5 +6,10 @@ namespace BurgerX.Infrastructure.Persistence;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(Product).Assembly);
+    }
+
     public DbSet<Product> Products { get; set; }
 }
---
+++ backend/BurgerX.Infrastructure/Persistence/EntityConfiguration/ProductConfiguration.cs
@@ -0,0 +1,17 @@
+using BurgerX.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BurgerX.Infrastructure.Persistence.EntityConfiguration;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(p => p.Id);
+        builder.HasQueryFilter(p => p.IsDeleted == false);
+    }
+}
