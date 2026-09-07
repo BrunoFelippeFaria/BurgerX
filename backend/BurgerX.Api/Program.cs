ec7dbdcc9e817ae1e09bab66ae84c9f3cebc502b
bruno <brunofelippefaria@gmail.com>
2026-09-07 18:10:24 -0300

    feat: adiciona conversor de enum

--- backend/BurgerX.Api/Program.cs
+++ backend/BurgerX.Api/Program.cs
@@ -1,9 +1,15 @@
+using System.Text.Json.Serialization;
+
 using BurgerX.Api.Middlewares;
 using BurgerX.CrossCutting.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDependencies(builder.Configuration);
-builder.Services.AddControllers();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(
+        new JsonStringEnumConverter()
+    ));
 
 var app = builder.Build();
 app.UseMiddleware<ExceptionHandler>();
