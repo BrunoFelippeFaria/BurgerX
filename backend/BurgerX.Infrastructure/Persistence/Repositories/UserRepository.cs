7acfb2fef1f5f26348ab31144aca4ae1698e56a2
bruno <brunofelippefaria@gmail.com>
2026-09-09 20:04:29 -0300

    feat: adiciona repositorio de usuarios

---
+++ backend/BurgerX.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+using BurgerX.Domain.Administration;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BurgerX.Infrastructure.Persistence.Repositories;
+
+public class UserRepository(AppDbContext context)
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task<User?> GetById(Guid id)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(u => u.Id == id);
+    }
+
+    public void Add(User user)
+    {
+        _context.Users.Add(user);
+    }
+}
